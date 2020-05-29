6bc85125cf19341b69917094ae95ae46d81035d1
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-28 21:05:04 -0500

    general layout
    Rough layout for application. Starting point.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: 'header.component.html'
+  templateUrl: 'header.component.html',
 })
-export class HeaderComponent{
-
+export class HeaderComponent {
+  // hamburger menu support
+  collapsed = true;
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://cdn.pixabay.com/photo/2017/11/08/15/34/recipe-2930786_960_720.jpg'
+    ),
+    new Recipe(
+      'Another Recipe',
+      'Yummy yum yum',
+      'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
