c96d1457a5fc364cb8be3527a87fd25bda7c6514
daisymae <daisymae.orcadiafarm@gmail.com>
2020-06-02 17:19:42 -0500

    ViewChild
    Using ViewChild to pass data from compnent to parent (in this case, adding shopping list items from the edit component to the parent list).

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() itemAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddItem() {
+    const itemName = this.nameInputRef.nativeElement.value;
+    const itemAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(itemName, itemAmount);
+    this.itemAdded.emit(newIngredient);
   }
-
 }
